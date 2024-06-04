41508f9cd863bc2a561f5195dc156efcc2069854
xiarong <xiarong@ipanel.com>
2024-06-04 10:50:18 +0800

    xiarong/

--- goProject/main.go
+++ goProject/main.go
@@ -19,6 +19,7 @@ func init() {
 // 包中init的执行顺序是包引入的顺序；
 // RunAfterConfInit 可控制加入的函数在配置初始化之后执行
 
+// 引用其他包，又希望其他包的初始化与本包的初始化有先后顺序，就可用在某操作前或后执行一些函数；
 func main() {
 	db.CreateUser()
 	handler.CreateHandler()
@@ -33,6 +34,10 @@ func main() {
 	http.HandleFunc("/msg", func(w http.ResponseWriter, r *http.Request) {
 		ctx := &context.Context{W: w, R: r}
 		ctx.ResponseString(200, "hello super man")
+	})
+	http.HandleFunc("/err", func(w http.ResponseWriter, r *http.Request) {
+		ctx := &context.Context{W: w, R: r}
+		ctx.ResonseJsonFailed(200, "something wrong")
 	})
 	http.ListenAndServe(":8081", nil)
 }
