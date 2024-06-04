962646b20635b9489149ee9475ecd4242d021d71
xiarong <xiarong@ipanel.com>
2024-06-04 13:39:46 +0800

    add

--- goProject/internal/server/router/context.go
+++ goProject/internal/server/router/context.go
@@ -1,4 +1,4 @@
-package context
+package router
 
 import (
 	"encoding/json"
--- goProject/main.go
+++ goProject/main.go
@@ -4,7 +4,6 @@ import (
 	"goproject/internal/conf"
 	"goproject/internal/db"
 	"goproject/internal/server/handler"
-	"goproject/internal/server/handler/context"
 	"goproject/internal/server/router"
 	"log"
 	"net/http"
@@ -26,17 +25,17 @@ func main() {
 	router.CreateRouter()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ctx := &context.Context{W: w, R: r}
+		ctx := &router.Context{W: w, R: r}
 		ctx.ResponseJsonOK(map[string]interface{}{
 			"token": "1234",
 		})
 	})
 	http.HandleFunc("/msg", func(w http.ResponseWriter, r *http.Request) {
-		ctx := &context.Context{W: w, R: r}
+		ctx := &router.Context{W: w, R: r}
 		ctx.ResponseString(200, "hello super man")
 	})
 	http.HandleFunc("/err", func(w http.ResponseWriter, r *http.Request) {
-		ctx := &context.Context{W: w, R: r}
+		ctx := &router.Context{W: w, R: r}
 		ctx.ResonseJsonFailed(200, "something wrong")
 	})
 	http.ListenAndServe(":8081", nil)
